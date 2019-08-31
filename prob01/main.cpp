e912eacae56ba40220f9b4761f02c3b8d0ba5140
arturitosilva20 <arturitosilva20@cs104u-2b.acad.fullerton.edu>
2019-08-31 13:30:45 -0700

    display diamond pattern

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,17 @@
-// Name: 
+// Name: Arturo Silva
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
+  std::cout <<"   *\n";
+  std::cout <<"  ***\n";
+  std::cout <<" *****\n";
+  std::cout <<"*******\n";
+  std::cout <<" *****\n";
+  std::cout <<"  ***\n";
+  std::cout <<"   *\n";
 
   return 0;
 }
